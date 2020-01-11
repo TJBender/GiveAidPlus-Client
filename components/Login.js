cfabcb57b72ecbff0ed8b53226f0529123aecbbd
TJBender <tiana.bender01@gmail.com>
2020-01-11 13:58:23 -0500

    working with login component

--- components/Login.js
+++ components/Login.js
@@ -1,1 +1,20 @@
 // child of MainContainer
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native'
+
+const Login = props => {
+    return ( <View style={styles.login}>
+        <Text style={styles.text}> This is da login page! </Text>
+    </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    login: {
+        padding: 10,
+        backgroundColor: 'lightpink'
+    },
+    text: {
+        color: 'white'
+    }
+})
