dcbac967410abc94a70f882be5350a101771411f
TJBender <tiana.bender01@gmail.com>
2020-01-13 16:34:15 -0500

    fetch and hooks functioning properly

--- App.js
+++ App.js
@@ -1,29 +1,47 @@
-import React from 'react';
-import NavBar from './components/NavBar' //dis right? ./?
-import MainContainer from './containers/MainContainer' //? yah? 
-import { StyleSheet, Text, View, Dimensions } from 'react-native';
+import React, { useState, useEffect} from 'react';
+import NavBar from './components/NavBar'
+import MainContainer from './containers/MainContainer'
+import { StyleSheet, SafeAreaView, FlatList, Text, View, ActivityIndicator, Dimensions} from 'react-native';
 
 export default function App() {
+  const [volunteers, setVolunteers] = useState([]);
+  // const [loading, setLoading] = useState(true);
+ 
+  useEffect(() => {
+    fetch("http://1b3ac74a.ngrok.io/volunteers")
+      .then(resp => resp.json())
+      .then(data => {
+        setVolunteers(data);
+      }).catch(function(error){
+        alert(error)
+      })
+  }, []) //if you increment page then the page variable would go in the array argument here.
 
   return (
-    <View style={styles.container}>
-      <Text style={{ color: 'black' }}>Login Puhlease</Text>
-      <View>
-        <Text style={{color:'black'}}> HELP </Text>
-      </View>
-      <View>
-        
-      </View>
-    </View>
+    // this should be the login page imported
+    <SafeAreaView style={styles.container}> 
+      <FlatList
+        data={volunteers}
+        keyExtractor={(item) => item.url }
+        renderItem={({ item }) => (
+          <View>
+            <Text>Name: {item.name}</Text>
+            <Text>Password: {item.password}</Text>
+          </View>
+        )}
+      />
+
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'white',
+    backgroundColor: 'lightblue',
     alignItems: 'center',
     justifyContent: 'center',
-  },
+  }
+
 });
 
--- components/NavBar.js
+++ components/NavBar.js
@@ -1,1 +1,14 @@
 // child of App
+import React from 'react';
+import { View, Text } from 'react-native';
+
+
+const NavBar = props => {
+    return (
+        <View>
+            <Text> This is the Nav Bar LOL </Text>
+        </View>
+    )
+}
+
+export default NavBar
